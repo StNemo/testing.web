d99546a7a83065663a67ce0502420842ba8a755b
Saint Nemo <cruz.rivera.victor@gmail.com>
2025-01-01 11:46:24 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -7,3 +7,19 @@ function myFunction() {
     x.className = "navbar";
   }
 } 
+
+/* Scroll to top button */
+var btn = $('#button-top');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, 300);
+});
